778f00968ead2e88722178c7b464ee354655e3ad
Angreh <angreh@gmail.com>
2016-11-08 12:10:00 -0200

    Atualizando

--- src/js/cases.js
+++ src/js/cases.js
@@ -2,6 +2,14 @@ $(function(){
 	$('.lb').colorbox({
 		iframe: true,
 		innerWidth: 640,
-		innerHeight: 390
+		innerHeight: 390,
+		maxHeight: '98%',
+		maxWidth: '98%',
+		speed: 0
+	});
+	$('.lbn').colorbox({
+		maxHeight: '90%',
+		maxWidth: '90%',
+		speed: 0
 	});
 });
