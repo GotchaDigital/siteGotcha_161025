0d7df5bd873e48bbf2813696807a18ed0457504f
Angreh <angreh@gmail.com>
2016-11-29 12:40:22 -0200

    Ajustando a ordem de carregamento das coisas

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,6 +56,8 @@ $(function()
 		}
 	);
 
+    $('#contact .map iframe').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.2492624126958!2d-46.687673649261676!3d-23.595391868660172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce574f4a1763db%3A0xdb4801501d7db420!2sR.+Olimp%C3%ADadas%2C+200+-+Itaim+Bibi%2C+S%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1477499422984');
+
     // FORMULARIO
     $('#contact form').submit(function()
     {
